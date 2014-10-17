af62480b13d5ceeffaa7c19dc6924122f076fc10
Vikram Haridas <vikram.haridas89@gmail.com>
2014-10-17 13:22:25 +0530

    Made home and about and help pages, will add the html later

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Confidr"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Confidr"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Confidr"
+  end
 end
