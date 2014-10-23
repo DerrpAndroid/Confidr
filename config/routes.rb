4626039108b5d67d8ef6881bffa25ad0b7afebb0
Vikram Haridas <vikram.haridas89@gmail.com>
2014-10-23 22:11:20 +0530

    please push

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'account_activations/edit'
+
   root   :to => 'static_pages#home'
   get    'home'    =>'static_pages#home'
   get    'help'    => 'static_pages#help'
@@ -12,6 +14,7 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
 
   resources :users
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
